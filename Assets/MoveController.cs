fd10586d3caeb8ea55d8f1af06b11d608f04e6f5
dvtai19871 <dvtai1987@gmail.com>
2023-08-07 00:12:43 +0700

    Up to lesson 10. Jump of character

--- Assets/MoveController.cs
+++ Assets/MoveController.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class MoveController : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
     private float xInput;
 
     // Start is called before the first frame update
@@ -19,6 +17,23 @@ public class MoveController : MonoBehaviour
     void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void Movement()
+    {
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
     }
 }
